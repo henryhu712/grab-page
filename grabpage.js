3f36e7b4213ee849efab71b488654facd140a7f5
Double H <h@obserbot.com>
2018-04-06 21:18:27 +0800

    Add the source of solution

--- grabpage.js
+++ grabpage.js
@@ -14,14 +14,15 @@ var url = system.args[1],
     resources = [],
     width = 400,
     height = 300;
-
 var page1 = require('webpage').create();
 var page2 = require('webpage').create();
-page1.viewportSize = { width:width, height:height }
 
+page1.viewportSize = { width:width, height:height }
 page1.open(url, function(stat) {
 
   if (stat == 'success') {
+    // https://stackoverflow.com/questions/31245553/how-to-get-the-height-of-a-full-html-page-in-phantomjs-javascript
+    // https://stackoverflow.com/a/31248055/3054511
     var realHeight = page1.evaluate(function(){
         return document.body.offsetHeight
     });
